cffcde21cd7ba0df1016ba6964f7b05874276b2b
zhang1998 <2764207312@qq.com>
2019-07-13 21:26:28 +0800

    origin

---
+++ Day01-15/test.py
@@ -0,0 +1,57 @@
+if username == "admin" and password == '1234567':
+    print("身份成功")
+else:
+    print("身份失败")
+
+
+if x>1:
+    x=3*x-5
+elif x>=   -1:
+    x=x+2
+else:
+    y=5*x+3
+
+
+
+#3
+
+x= input('请输入长度')
+dd=input('请输入单位')
+if dd == 'cm':
+    print('%f英寸=%f厘米' % (calue , calue ))
+
+
+
+#4
+from random import randint
+
+face = randint (1,6)
+if face==1:
+    print ('跳舞')
+if face==2:
+    print ('吃鱼')
+if face==3:
+    print ('喝水')
+
+
+    x=input
+#5
+x=float (input ('a='))
+if x>=90:
+    print ('A')
+elif x>=60:
+    print ('B')
+#6
+import math
+x= input ('please input 长')
+y=float (input ('请输入宽'))
+
+if a+b>c and a+c>b and c+b>a:
+    print ('周长是%f' % (x+y+z))
+    print ('面积是%f' % (x*y*z))
+    math.sqrt ()
+else:
+    print ("错误")
+
+
+    #8
